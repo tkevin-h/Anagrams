4048c49e09de9afe9c3d0368878624fe5e2f9e81
thavin <tkevin.horng@gmail.com>
2020-08-31 23:51:20 +1000

    Adding test for letter count

--- src/main/java/com/thavin/CheckAnagrams/CompareWords.java
+++ src/main/java/com/thavin/CheckAnagrams/CompareWords.java
@@ -1,7 +1,12 @@
 package com.thavin.CheckAnagrams;
 
 public class CompareWords {
+
     public boolean checkAnagram(String wordOne, String wordTwo) {
         return wordOne.equals(wordTwo);
+    }
+
+    public boolean checkLettersAmount(String wordOne, String wordTwo) {
+        return wordOne.length() == wordTwo.length();
     }
 }
--- src/test/java/com/thavin/CheckAnagrams/CompareWordsTest.java
+++ src/test/java/com/thavin/CheckAnagrams/CompareWordsTest.java
@@ -4,13 +4,22 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class CompareWordsTest {
+
     @Test
     public void words_are_equal() {
         CompareWords words = new CompareWords();
-
         String a = "a";
         String b = "a";
 
         assertTrue(words.checkAnagram(a, b), "Words are not the same.");
+    }
+
+    @Test
+    public void number_of_letters_unequal() {
+        CompareWords words = new CompareWords();
+        String a = "a";
+        String b = "ab";
+
+        assertFalse(words.checkLettersAmount(a, b), "Letters count are the same.");
     }
 }
