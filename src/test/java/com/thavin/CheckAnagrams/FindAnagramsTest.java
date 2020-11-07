ef3c24e54f50dca31c3fc514c9464dfa4597a93d
thavin <tkevin.horng@gmaill.com>
2020-11-07 16:20:34 +1100

    Fixing reading text file for dictionary by using relative path.

--- src/test/java/com/thavin/CheckAnagrams/FindAnagramsTest.java
+++ src/test/java/com/thavin/CheckAnagrams/FindAnagramsTest.java
@@ -1,10 +1,7 @@
 package com.thavin.CheckAnagrams;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-import com.sun.source.tree.Tree;
 import org.junit.Test;
-
 import java.io.IOException;
 import java.util.Set;
 import java.util.TreeSet;
@@ -12,7 +9,7 @@ import java.util.TreeSet;
 public class FindAnagramsTest {
     //Positive cases
     @Test
-    void find_shouldReturnArrayOfWords_whenWordIsAnagram() {
+    public void find_shouldReturnArrayOfWords_whenWordIsAnagram() {
         Set<String> words = new TreeSet<>();
         words.add("abc");
         words.add("acb");
@@ -23,14 +20,14 @@ public class FindAnagramsTest {
 
         String[] wordCombinations = new String[] {"cab"};
 
-        //assertArrayEquals(wordCombinations, find(words));
+        assertArrayEquals(wordCombinations, find(words));
     }
 
     @Test
-    void buildDictionary_shouldReturnSet_whenGivenFileName() throws IOException {
+    public void buildDictionary_shouldReturnSet_whenGivenFileName() throws IOException {
         Set<String> dictionary = new TreeSet<>();
         dictionary.add("abc");
 
-        assertEquals(dictionary, FindAnagrams.buildDictionary("test_words.txt"));
+        assertEquals(dictionary, FindAnagrams.buildDictionary("./resources/test_words.txt"));
     }
 }
